3a93c3836eb7e1bde1ac2254e9652ebd5f748e9d
asim <asimjaipuri99@gmail.com>
2021-02-21 15:35:16 +0530

    initial commit

--- App.js
+++ App.js
@@ -1,21 +1,67 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, View, FlatList, Alert } from 'react-native';
+import Header from './components/Header';
+import uuid from 'uuid';
+import ListItem from './components/ListItem';
+import AddItem from './components/AddItem';
 
-export default function App() {
+const App = () => {
+  const [items, setitems] = useState([
+    {
+      id: uuid(),
+      text: 'Milk',
+    },
+    {
+      id: uuid(),
+      text: 'Eggs',
+    },
+    {
+      id: uuid(),
+      text: 'Cake',
+    },
+    {
+      id: uuid(),
+      text: 'Soda',
+    },
+  ]);
+
+  const deleteItem = (id) => {
+    setitems((prevItem) => {
+      return prevItem.filter((item) => item.id !== id);
+    });
+  };
+
+  const addItem = (text) => {
+    if (!text) {
+      Alert.alert('Error', 'Please Enter An Item', {
+        text: 'Ok',
+      });
+    } else {
+      setitems((prevItems) => {
+        return [{ id: uuid(), text }, ...prevItems];
+      });
+    }
+  };
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Header title='Todo App' />
+      <AddItem addItem={addItem} />
+      <FlatList
+        data={items}
+        renderItem={({ item }) => (
+          <ListItem item={item} deleteItem={deleteItem} />
+        )}
+      />
+      <StatusBar style='auto' />
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
+
+export default App;
