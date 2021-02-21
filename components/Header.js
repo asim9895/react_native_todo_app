6d35346fb97ce8d31e800fc2df33ffa0104c9796
asim <asimjaipuri99@gmail.com>
2021-02-21 15:37:08 +0530

    adding todo functionality

---
+++ components/Header.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { StyleSheet, View, Text } from 'react-native';
+
+const Header = ({ title }) => {
+  return (
+    <View style={styles.header}>
+      <Text style={styles.text}>{title}</Text>
+    </View>
+  );
+};
+
+Header.defaultProps = {
+  title: 'Todo App',
+};
+
+const styles = StyleSheet.create({
+  header: {
+    height: 80,
+    padding: 15,
+    backgroundColor: 'darkslateblue',
+  },
+  text: {
+    color: '#fff',
+    fontSize: 23,
+    textAlign: 'center',
+    paddingTop: 20,
+  },
+});
+
+export default Header;
